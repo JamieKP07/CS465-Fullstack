600f0405042b918a2e727b02fbd12b9c38fd126b
JamieKP07 <jamie.kutzer@snhu.edu>
2026-08-02 21:23:48 -0500

    Completed Module 5 REST API implementation

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var hbs = require('hbs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('./app_api/models/db');
+
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 
 var app = express();
 
@@ -25,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/travel', travelRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,14 +1,38 @@
-const fs = require('fs');
+const tripsEndpoint = 'http://localhost:3000/api/trips';
 
-const trips = JSON.parse(
-    fs.readFileSync('./data/trips.json', 'utf8')
-);
+const options = {
+    method: 'GET',
+    headers: {
+        Accept: 'application/json'
+    }
+};
 
-const travel = (req, res) => {
-    res.render('travel', {
-        title: 'Travlr Getaways',
-        trips
-    });
+const travel = async (req, res) => {
+    try {
+        const response = await fetch(tripsEndpoint, options);
+
+        if (!response.ok) {
+            throw new Error(`API request failed with status ${response.status}`);
+        }
+
+        const trips = await response.json();
+
+        if (!Array.isArray(trips)) {
+            throw new Error('API response was not a valid trip collection');
+        }
+
+        res.render('travel', {
+            title: 'Travlr Getaways',
+            trips,
+            message: trips.length === 0 ? 'No trips are currently available.' : null
+        });
+    } catch (err) {
+        res.status(500).render('travel', {
+            title: 'Travlr Getaways',
+            trips: [],
+            message: err.message
+        });
+    }
 };
 
 module.exports = {
